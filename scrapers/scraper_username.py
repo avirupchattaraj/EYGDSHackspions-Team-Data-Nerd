e9c5a36562ceb0827db7e9da815b9aee062bb94b
Avirup Chattaraj <avi14chattarajyu@gmail.com>
2020-11-25 12:48:19 +0530

    segregate scrapers based upon use cases-wip

--- scrapers/scraper_username.py
+++ scrapers/scraper_username.py
@@ -2,11 +2,10 @@ import twint
 
 # Configure
 c = twint.Config()
-c.Username = "realDonaldTrump"
-c.Search = "great"
+c.Username = "EY_India"
 c.Limit = 3
 c.Store_csv = True
 c.Output = "output.csv"
 
 # Run
-twint.run.Search(c)
+twint.run.Profile(c)
