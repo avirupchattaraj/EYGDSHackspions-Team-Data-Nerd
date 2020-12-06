4a6f0697c2fd82ad5a66f936808bd00a9fae5cc9
Avirup Chattaraj <avi14chattarajyu@gmail.com>
2020-12-06 21:07:32 +0530

    configured for static sites

--- EYSociolytics/website/views.py
+++ EYSociolytics/website/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+def home(request):
+    return render(request,"website/home.html")
+
+def about(request):
+    return render(request,"website/about.html")
+
+def pricing(request):
+    return render(request,"website/pricing.html")
+
+def contact(request):
+    return render(request,"website/contact.html")
+
+def download(request):
+    return render(request,"website/download.html")
+    
