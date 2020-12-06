71ca428d368db633c3a618c8e74d9af2f433310c
Avirup Chattaraj <avi14chattarajyu@gmail.com>
2020-12-06 21:05:22 +0530

    scrapers for twint for different purpose

---
+++ EYSociolytics/twitteranalysis/scrapers/scraper2.py
@@ -0,0 +1,23 @@
+import twint
+from datetime import datetime
+current_time=datetime.today()
+import os
+from pathlib import Path
+path=os.path.join(Path(__file__).resolve().parent.parent,"generated")
+
+day=current_time.day
+month=current_time.month
+year=current_time.year
+hour=current_time.hour
+minute=current_time.minute
+second=current_time.second
+
+
+def csv_creator_two(search):
+    c = twint.Config()
+    c.Search= search
+    c.Limit = 150
+    c.Store_csv = True
+    c.Output = f"output_{day}_{month}_{year}_{hour}_{minute}_{second}.csv"
+    
+    twint.run.Search(c)
