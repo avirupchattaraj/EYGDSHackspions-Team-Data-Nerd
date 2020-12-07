e55d036c8e015b0eaf2d9636423520f0f7753e2c
Avirup Chattaraj <avi14chattarajyu@gmail.com>
2020-12-07 15:26:24 +0530

    update views to display the sentiment values as json

--- EYSociolytics/twitteranalysis/views.py
+++ EYSociolytics/twitteranalysis/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .scrapers import scraper1,scraper2,scraper3
+from .nlpengine import nlpenginetwitter
 
+sentiment_values=''
 # Create your views here.
 def twitterhome(request):
     return render(request,'twitteranalysis/home.html')
@@ -12,14 +14,18 @@ def inputform1(request):
     if request.method=='POST':
         username=request.POST.get("username")
         scraper1.csv_creator_one(username)
+        global sentiment_values
+        sentiment_values=nlpenginetwitter.sentiment_average_calculator()
         return redirect("result")
-
+    
     return render(request,"twitteranalysis/inputform1.html")
 
 def inputform2(request):
     if request.method=='POST':
         search=request.POST.get("search")
         scraper2.csv_creator_two(search)
+        global sentiment_values
+        sentiment_values=nlpenginetwitter.sentiment_average_calculator()
         return redirect("result")
 
     return render(request,"twitteranalysis/inputform2.html")
@@ -32,6 +38,8 @@ def inputform3(request):
         until=request.POST.get("until")
         limit=request.POST.get("limit")
         scraper3.csv_creator_three(username,keyword,since,until,limit)
+        global sentiment_values
+        sentiment_values=nlpenginetwitter.sentiment_average_calculator()
         return redirect("result")
     return render(request,"twitteranalysis/inputform3.html")
 
@@ -39,8 +47,14 @@ def inputform4(request):
     if request.method=='POST':
         username=request.POST.get("username")
         scraper2.csv_creator_two(username)
+        global sentiment_values
+        sentiment_values=nlpenginetwitter.sentiment_average_calculator()
         return redirect("result")
     return render(request,"twitteranalysis/inputform4.html")
 
 def result(request):
-    return render(request,"twitteranalysis/result.html")
+    context={
+        'sentiment':sentiment_values
+    }
+    print(context)
+    return render(request,"twitteranalysis/result.html",context)
